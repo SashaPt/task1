2222792b05e80b8a95c843a42fda9c4d5be45e3c
SashaPt <aleksa.toc18@gmail.com>
2022-07-20 10:34:45 +0300

    Created ButtonProps component

---
+++ src/components/ButtonProps/ButtonProps.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ButtonProps.css';
+
+const ButtonProps = () => {
+  return <button className="new-button">New Button</button>;
+  
+};
+
+export default ButtonProps;
